e35ccc4824abd8bad833bc8d9f4b112648e5066e
berkpak <pakkberk@gmail.com>
2022-11-26 13:44:42 +0300

    first

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import UserResults from '../components/layout/users/UserResults';
+import UserSearch from '../components/layout/users/UserSearch';
+
+function Home() {
+  return (
+    <>
+      <UserSearch />
+      <UserResults />
+    </>
+  );
+}
+
+export default Home;
